c341ac13588cedf2e607eb7927097297c7b36236
ajogerio <aaogerio@up.edu.ph>
2025-12-24 12:36:10 +0800

    feat: add gravity

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -1,14 +1,20 @@
 extends CharacterBody2D
 
-const SPEED = 300
+const HORIZONTAL_SPEED = 300
+const GRAVITY_SPEED = 500
 
 func _physics_process(delta: float) -> void:
+	# horizontal movement:
 	velocity.x = 0
 	
 	if Input.is_action_pressed("move_left"):
-		velocity.x -= SPEED
+		velocity.x -= HORIZONTAL_SPEED
 	
 	if Input.is_action_pressed("move_right"):
-		velocity.x += SPEED
+		velocity.x += HORIZONTAL_SPEED
 	
+	# gravity movement:
+	if not is_on_floor():
+		velocity.y += GRAVITY_SPEED * delta
+		
 	move_and_slide()
