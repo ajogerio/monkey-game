bdb94cfa2076d7e1e032d151c65e50e6ce735cd0
ajogerio <aaogerio@up.edu.ph>
2025-12-28 21:10:49 +0800

    feat: add left and right drag margins

--- Scripts/camera.gd
+++ Scripts/camera.gd
@@ -1,9 +1,8 @@
 extends Camera2D
 
-func _ready() -> void:
-	drag_horizontal_enabled = true
-	drag_vertical_enabled = false
-	
-	# set the drag margins
-	drag_left_margin = 0.3
-	drag_right_margin = 0.3
+#func _ready() -> void:
+	#drag_horizontal_enabled = true
+	#drag_vertical_enabled = false
+	#
+	## set the drag margins
+	#drag_left_margin = 0.1
