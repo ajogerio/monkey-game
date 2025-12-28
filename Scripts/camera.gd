c6ed2279fae4152f07538b88b03e0ee0a8158857
ajogerio <aaogerio@up.edu.ph>
2025-12-28 20:55:17 +0800

    feat: add godot icon for movement reference

--- Scripts/camera.gd
+++ Scripts/camera.gd
@@ -3,3 +3,7 @@ extends Camera2D
 func _ready() -> void:
 	drag_horizontal_enabled = true
 	drag_vertical_enabled = false
+	
+	# set the drag margins
+	drag_left_margin = 0.3
+	drag_right_margin = 0.3
