2d9f5d77b00d4da03b4ee1b4a0e8cc927467fb46
ajogerio <aaogerio@up.edu.ph>
2025-12-29 22:58:35 +0800

    feat: add script that works for small screen but not for big screen

--- Scripts/camera.gd
+++ Scripts/camera.gd
@@ -1,8 +1,15 @@
 extends Camera2D
 
-#@export var player_path: NodePath
-#@onready var player: Node2D = get_node(player_path)
-#
-#func _process(_delta):
-	## Track the player so margins can apply
-	#position = player.position
+@export var player_node: Node2D
+@export var deadzone_percent: float = 0.4
+
+
+func _process(delta: float) -> void:
+	if not player_node:
+		return
+	
+	var viewport_width = get_viewport().size.x
+	var deadzone_width = viewport_width * deadzone_percent
+	
+	if player_node.global_position.x > deadzone_width:
+		global_position.x += player_node.velocity.x * delta
