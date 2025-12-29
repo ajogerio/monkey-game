5bb99ac8d39a87257f990d98d5631487208059e6
ajogerio <aaogerio@up.edu.ph>
2025-12-29 23:31:05 +0800

    wip: fix the camera panning

--- Scripts/camera.gd
+++ Scripts/camera.gd
@@ -1,15 +1,15 @@
 extends Camera2D
 
-@export var player_node: Node2D
-@export var deadzone_percent: float = 0.4
-
+@export var player_node: CharacterBody2D
+@export var deadzone_world: float = 45  # world units
 
 func _process(delta: float) -> void:
 	if not player_node:
 		return
 	
 	var viewport_width = get_viewport().size.x
-	var deadzone_width = viewport_width * deadzone_percent
+	var viewport_left_edge_position = global_position.x - (viewport_width / 2)
+	var player_distance_from_viewport_left_edge = player_node.global_position.x - viewport_left_edge_position
 	
-	if player_node.global_position.x > deadzone_width:
+	if player_distance_from_viewport_left_edge > deadzone_world:
 		global_position.x += player_node.velocity.x * delta
