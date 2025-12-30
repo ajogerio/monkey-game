832aff0a2eb45ed8d0119c0a99804ab51a6e452c
ajogerio <aaogerio@up.edu.ph>
2025-12-30 20:43:44 +0800

    fix: camera pans to the left and right when the player pushes past the deadzone barriers

--- Scripts/camera.gd
+++ Scripts/camera.gd
@@ -1,19 +1,20 @@
 extends Camera2D
 
 @export var player_node: CharacterBody2D
-@export var deadzone_percent: float = 0.3
-@export var camera_follow_speed: float = 20.0
-
+@export var right_barrier_percent: float = 0.3
+@export var left_barrier_percent: float = 0.1
+	
 func _process(delta: float) -> void:
 	if not player_node:
 		return
 	
 	var viewport_width = get_viewport_rect().size.x
-	var move_camera_when_player_passes_this_barrier = global_position.x + (viewport_width * deadzone_percent)
-	var player_position_x = player_node.position.x
+	var right_barrier_of_deadzone = global_position.x + (viewport_width * right_barrier_percent)
+	var left_barrier_of_deadzone = global_position.x + (viewport_width * left_barrier_percent)
 	
-	if player_position_x > move_camera_when_player_passes_this_barrier:
-		var overshoot_distance_of_player_from_barrier = player_node.global_position.x - (viewport_width * deadzone_percent)
-		
-		# smoothly transition the camera to close the overshoot of the player from the barrier
-		global_position.x = lerp(global_position.x, overshoot_distance_of_player_from_barrier, camera_follow_speed * delta)
+	# make the camera pan to the right whenever the player is past the right barrier of the deadzone
+	if player_node.position.x > right_barrier_of_deadzone || player_node.position.x < left_barrier_of_deadzone:
+		global_position.x += player_node.velocity.x * delta
+	else:
+		# player is in between the barriers
+		return
