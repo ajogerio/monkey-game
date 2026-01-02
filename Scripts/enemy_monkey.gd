5fff5a229db06582b7d68897afea2c547fa059db
ajogerio <aaogerio@up.edu.ph>
2026-01-02 20:03:21 +0800

    chore: clean up code

--- Scripts/enemy_monkey.gd
+++ Scripts/enemy_monkey.gd
@@ -9,8 +9,10 @@ var is_pacing_right : bool = false
 var is_pacing_left : bool = false
 var pace_direction = "right"
 
+@onready var animation_player: AnimationPlayer = $"Monkey Sprite/AnimationPlayer"
+
 func _ready() -> void:
-	$"Monkey Sprite"/AnimationPlayer.play("idle_left")
+	animation_player.play("idle_left")
 func _physics_process(delta: float) -> void:
 	if not is_on_floor():
 		velocity.y += GRAVITY_SPEED * delta
@@ -21,7 +23,7 @@ func _physics_process(delta: float) -> void:
 		if position.x - start_position >= PACE_DISTANCE:
 			velocity.x = 0
 			is_pacing_right = false
-			$"Monkey Sprite"/AnimationPlayer.play("idle")
+			animation_player.play("idle")
 			pace_direction = "left"
 	
 	if is_pacing_left:
@@ -30,7 +32,7 @@ func _physics_process(delta: float) -> void:
 		if position.x - start_position <= -PACE_DISTANCE:
 			velocity.x = 0
 			is_pacing_left = false
-			$"Monkey Sprite"/AnimationPlayer.play("idle_left")
+			animation_player.play("idle_left")
 			pace_direction = "right"
 		
 	move_and_slide()
@@ -38,11 +40,13 @@ func _physics_process(delta: float) -> void:
 # set a timer
 func _on_timer_timeout():
 	if not is_pacing_right && not is_pacing_left:
+		
 		start_position = position.x
+		
 		if pace_direction == "right":
-			# make monkey pace to the right
 			is_pacing_right = true
-			$"Monkey Sprite"/AnimationPlayer.play("walk_right")
+			animation_player.play("walk_right")
+			
 		if pace_direction == "left":
 			is_pacing_left = true
-			$"Monkey Sprite"/AnimationPlayer.play("walk_left")
+			animation_player.play("walk_left")
