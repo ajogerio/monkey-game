d72d263e3b339d94a733f0ce512202f26a8bcc86
ajogerio <aaogerio@up.edu.ph>
2026-01-02 19:26:48 +0800

    feat: add monkey pacing left

--- Scripts/enemy_monkey.gd
+++ Scripts/enemy_monkey.gd
@@ -1,11 +1,13 @@
 extends CharacterBody2D
 
 const GRAVITY_SPEED = 1000
-const HORIZONTAL_SPEED = 1
+const HORIZONTAL_SPEED = 50
+const PACE_DISTANCE = 50
 
 var start_position : float
 var is_pacing_right : bool = false
 var is_pacing_left : bool = false
+var pace_direction = "right"
 
 func _ready() -> void:
 	$"Monkey Sprite"/AnimationPlayer.play("idle_left")
@@ -14,29 +16,33 @@ func _physics_process(delta: float) -> void:
 		velocity.y += GRAVITY_SPEED * delta
 	
 	if is_pacing_right:
-		velocity.x += HORIZONTAL_SPEED
+		velocity.x = HORIZONTAL_SPEED
 		
-		if position.x - start_position >= 50:
+		if position.x - start_position >= PACE_DISTANCE:
 			velocity.x = 0
 			is_pacing_right = false
-			$"Monkey Sprite"/AnimationPlayer.play("idle_left")
+			$"Monkey Sprite"/AnimationPlayer.play("idle")
+			pace_direction = "left"
 	
 	if is_pacing_left:
-		velocity.x = HORIZONTAL_SPEED
+		velocity.x = -HORIZONTAL_SPEED
+		
+		if position.x - start_position <= -PACE_DISTANCE:
+			velocity.x = 0
+			is_pacing_left = false
+			$"Monkey Sprite"/AnimationPlayer.play("idle_left")
+			pace_direction = "right"
 		
 	move_and_slide()
 	
 # set a timer
 func _on_timer_timeout():
-	# after the timer play the walk animation
-	$"Monkey Sprite"/AnimationPlayer.play("walk_right")
-	
-	# make monkey pace to the right
-	start_position = position.x
-	is_pacing_right = true
-	# play the idle animation
-	# wait for the timer again
-	# play the walk animation
-	# move the monkey to the left
-	# stop
-	# repeat step one
+	if not is_pacing_right && not is_pacing_left:
+		start_position = position.x
+		if pace_direction == "right":
+			# make monkey pace to the right
+			is_pacing_right = true
+			$"Monkey Sprite"/AnimationPlayer.play("walk_right")
+		if pace_direction == "left":
+			is_pacing_left = true
+			$"Monkey Sprite"/AnimationPlayer.play("walk_right")
