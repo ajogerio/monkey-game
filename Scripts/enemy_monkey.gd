4870205fc9bf7156e3997db6154aa5991a9da0aa
ajogerio <aaogerio@up.edu.ph>
2026-01-02 17:41:24 +0800

    feat: add gravity to monkey

--- Scripts/enemy_monkey.gd
+++ Scripts/enemy_monkey.gd
@@ -1,5 +1,9 @@
 extends CharacterBody2D
 
+const GRAVITY_SPEED = 1000
 
 func _physics_process(delta: float) -> void:
+	if not is_on_floor():
+		velocity.y += GRAVITY_SPEED * delta
+	
 	move_and_slide()
