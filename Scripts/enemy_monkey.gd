3d1f9c4a4ae1f5de0f3c0a3d560b3ef3044a73ec
ajogerio <aaogerio@up.edu.ph>
2026-01-02 17:44:19 +0800

    feat: play monkey idle animation

--- Scripts/enemy_monkey.gd
+++ Scripts/enemy_monkey.gd
@@ -2,6 +2,8 @@ extends CharacterBody2D
 
 const GRAVITY_SPEED = 1000
 
+func _ready() -> void:
+	$"Monkey Sprite"/AnimationPlayer.play("idle")
 func _physics_process(delta: float) -> void:
 	if not is_on_floor():
 		velocity.y += GRAVITY_SPEED * delta
