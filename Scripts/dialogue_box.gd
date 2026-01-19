fd4f666ceb8ead6b233c8e6bb611170e8736afb0
ajogerio <aaogerio@up.edu.ph>
2026-01-19 22:30:52 +0800

    feat: add typewriter effect

--- Scripts/dialogue_box.gd
+++ Scripts/dialogue_box.gd
@@ -1,11 +1,30 @@
 extends Control
 
 # references to the child nodes
-@onready var speaker_name_node = $"Panel/MarginContainer/HBoxContainer/VBoxContainer/Speaker Name"
-@onready var speaker_dialogue_node = $"Panel/MarginContainer/HBoxContainer/VBoxContainer/Speaker Dialogue"
+@onready var speaker_name = $"Panel/MarginContainer/HBoxContainer/VBoxContainer/Speaker Name"
+@onready var speaker_dialogue = $"Panel/MarginContainer/HBoxContainer/VBoxContainer/Speaker Dialogue"
 
-func show_dialogue(speaker: String, dialogue: String) -> void:
+var typing_speed := 0.03
+var is_typing := false
+
+func show_dialogue(speaker: String, text: String) -> void:
 	visible = true
-	speaker_name_node.text = speaker
-	speaker_dialogue_node.clear()
-	speaker_dialogue_node.append_bbcode(dialogue)
+	speaker_name.text = speaker
+	
+	speaker_dialogue.clear()
+	speaker_dialogue.append_text(text)
+	speaker_dialogue.visible_characters = 0
+	
+	is_typing = true
+	_start_typing()
+
+func _start_typing() -> void:
+	while speaker_dialogue.visible_characters < speaker_dialogue.get_total_character_count():
+		speaker_dialogue.visible_characters += 1
+		await get_tree().create_timer(typing_speed).timeout # wait for typing_speed units before going to next line
+	
+	is_typing = false
+
+
+func _on_ready() -> void:
+	show_dialogue("Snorlax", "Hello there. This text should type slowly.")
