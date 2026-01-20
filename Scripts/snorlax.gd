14079b2a3859e701300784001846b06cda3c0c6a
ajogerio <aaogerio@up.edu.ph>
2026-01-20 19:51:59 +0800

    feat: show floating E key if the player is in the interactable zone and hide if not

--- Scripts/snorlax.gd
+++ Scripts/snorlax.gd
@@ -1,9 +1,16 @@
 extends Area2D
 
+@onready var floating_e_key = $"Floating E Key"
+
 var is_player_in_talk_range := false
 
 func _on_body_entered(body: Node2D) -> void:
 	if body.is_in_group("player"):
 		print("player entered the interaction zone.")
 		is_player_in_talk_range = true
-		
+		floating_e_key.visible = true
+
+func _on_body_exited(body: Node2D) -> void:
+	if body.is_in_group("player"):
+		is_player_in_talk_range = false
+		floating_e_key.visible = false
