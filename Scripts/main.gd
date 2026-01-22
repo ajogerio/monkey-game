aa093c6cb80f38e31cf15cc4f378d6399d90490d
ajogerio <aaogerio@up.edu.ph>
2026-01-22 20:52:20 +0800

    feat: code placing the player node in the spawn point

--- Scripts/main.gd
+++ Scripts/main.gd
@@ -5,7 +5,7 @@ const TUTORIAL_LEVEL: PackedScene = preload("res://Scenes/Levels/tutorial_level_
 @onready var level_container = $Level
 @onready var player = $Player
 
-func _on_ready() -> void:
+func _ready() -> void:
 	load_level(TUTORIAL_LEVEL)
 
 func load_level(level_scene: PackedScene):
@@ -13,7 +13,12 @@ func load_level(level_scene: PackedScene):
 	for child in level_container.get_children():
 		child.queue_free()
 	
-	# load the new level scene
+	# instantiate the level scene into the scene tree
 	var level_instance: Node = level_scene.instantiate()
 	level_container.add_child(level_instance)
 	
+	# move the player to the spawn point
+	if level_instance.has_node("Player Spawn"):
+		var spawn := level_instance.get_node("Player Spawn") as Marker2D
+		if spawn:
+			player.global_position = spawn.global_position
