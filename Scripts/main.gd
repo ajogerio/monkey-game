98b7db0c47d183f88541213ff2446b5a7c21736d
ajogerio <aaogerio@up.edu.ph>
2026-01-22 20:43:52 +0800

    feat: add code for loading the level

--- Scripts/main.gd
+++ Scripts/main.gd
@@ -8,8 +8,12 @@ const TUTORIAL_LEVEL: PackedScene = preload("res://Scenes/Levels/tutorial_level_
 func _on_ready() -> void:
 	load_level(TUTORIAL_LEVEL)
 
-func load_level(scene_path: PackedScene):
+func load_level(level_scene: PackedScene):
 	# clear any existing levels
 	for child in level_container.get_children():
 		child.queue_free()
+	
+	# load the new level scene
+	var level_instance: Node = level_scene.instantiate()
+	level_container.add_child(level_instance)
 	
