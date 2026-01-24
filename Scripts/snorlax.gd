6946266d15e8c7735bd99adedb90212aca31e8a0
ajogerio <aaogerio@up.edu.ph>
2026-01-24 09:40:31 +0800

    fix: npc script not receiving dialogue finished signal with autoload

--- Scripts/snorlax.gd
+++ Scripts/snorlax.gd
@@ -1,7 +1,6 @@
 extends Area2D
 
 @onready var floating_e_key = $"Floating E Key"
-@onready var dialogue_box = get_tree().get_first_node_in_group("dialogue_box")
 
 var is_player_in_talk_range := false
 var is_talking := false
@@ -30,9 +29,12 @@ func _on_body_exited(body: Node2D) -> void:
 func _start_dialogue():
 	floating_e_key.visible = false
 	is_talking = true
-	dialogue_box.show_dialogue(dialogue_data)
+	DialogueManagerAutoload.dialogue_box.show_dialogue(dialogue_data)
 
 func _on_dialogue_box_dialogue_finished() -> void:
 	is_talking = false
 	if is_player_in_talk_range:
 		floating_e_key.visible = true
+
+func _ready():
+	DialogueManagerAutoload.dialogue_finished.connect(_on_dialogue_box_dialogue_finished)
