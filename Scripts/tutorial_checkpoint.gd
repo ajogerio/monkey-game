886a3fc6e91cfd8a7fad646a51e5500b997542b7
ajogerio <aaogerio@up.edu.ph>
2026-01-27 19:55:23 +0800

    feat: add double jump dialogue

--- Scripts/tutorial_checkpoint.gd
+++ Scripts/tutorial_checkpoint.gd
@@ -2,6 +2,8 @@ extends Area2D
 
 @export var dialogue_filepath: String
 
+
+
 func _on_body_entered(body: Node2D) -> void:
 	if body.is_in_group("player"):
 		DialogueManagerAutoload.dialogue_box.show_dialogue(dialogue_filepath)
