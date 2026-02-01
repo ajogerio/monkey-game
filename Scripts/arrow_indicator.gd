88d1ff8692efa0a7ab28ed99188ad63f520ecd18
ajogerio <aaogerio@up.edu.ph>
2026-02-01 13:16:47 +0800

    feat: add floating arrows on checkpoints

---
+++ Scripts/arrow_indicator.gd
@@ -0,0 +1,29 @@
+extends Node2D
+
+@export var float_height := 10.0
+@export var float_time := 0.8
+
+var start_y: float
+
+func _ready():
+	start_y = position.y
+	start_hover()
+
+func start_hover():
+	var tween = create_tween()
+	
+	tween.set_loops() # make this tween run forever
+	
+	tween.tween_property(
+		self,
+		"position:y",
+		start_y - float_height,
+		float_time
+	).set_trans(Tween.TRANS_SINE).set_ease(Tween.EASE_IN_OUT)
+	
+	tween.tween_property(
+		self,
+		"position:y",
+		start_y,
+		float_time
+	).set_trans(Tween.TRANS_SINE).set_ease(Tween.EASE_IN_OUT)
