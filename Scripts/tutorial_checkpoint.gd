510eb7f2864e4ee1e0cd21454fb3c67b56652878
ajogerio <aaogerio@up.edu.ph>
2026-02-01 13:33:24 +0800

    feat: add checkpoint name export in checkpoint script

--- Scripts/tutorial_checkpoint.gd
+++ Scripts/tutorial_checkpoint.gd
@@ -1,8 +1,7 @@
 extends Area2D
 
+@export var checkpoint_name := "Checkpoint1"
 @export var dialogue_filepath: String
-
-
 
 func _on_body_entered(body: Node2D) -> void:
 	if body.is_in_group("player"):
