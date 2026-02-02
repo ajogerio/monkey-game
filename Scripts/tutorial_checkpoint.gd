fbc8b33ab8d965eb07d88cddcbfcc2ff456fad62
ajogerio <aaogerio@up.edu.ph>
2026-02-02 21:00:46 +0800

    feat: show next arrow after entering checkpoint zone

--- Scripts/tutorial_checkpoint.gd
+++ Scripts/tutorial_checkpoint.gd
@@ -1,6 +1,7 @@
 extends Area2D
 
-@export var checkpoint_name := "Checkpoint1"
+@export var checkpoint_name: String = ""
+@export var next_checkpoint: String= ""
 @export var dialogue_filepath: String
 
 @onready var arrow_indicator := $"Arrow Indicator"
@@ -8,7 +9,9 @@ extends Area2D
 func _ready():
 	# Hide the arrow indicator at start
 	arrow_indicator.hide_arrow()
-	SignalBusAutoload.connect("show_arrow_signal", Callable(self, "_on_show_arrow"))
+	
+	SignalBusAutoload.show_arrow_signal.connect(Callable(self, "_on_show_arrow"))
+	DialogueManagerAutoload.dialogue_finished.connect(Callable(self, "_on_dialogue_finished"))
 	
 func _on_body_entered(body: Node2D) -> void:
 	if body.is_in_group("player"):
@@ -17,3 +20,7 @@ func _on_body_entered(body: Node2D) -> void:
 func _on_show_arrow(name: String):
 	if name == checkpoint_name:
 		arrow_indicator.show_arrow()
+		
+func _on_dialogue_finished():
+	if next_checkpoint != "":
+		SignalBusAutoload.show_arrow_signal.emit(next_checkpoint)
