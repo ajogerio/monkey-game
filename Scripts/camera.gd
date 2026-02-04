f14dc301a9809e6291a6b91ad961c435631cf919
ajogerio <aaogerio@up.edu.ph>
2026-02-04 19:53:17 +0800

    feat: compute camera bounds

--- Scripts/camera.gd
+++ Scripts/camera.gd
@@ -1,8 +1,22 @@
 extends Camera2D
 
 @export var player_node: CharacterBody2D
+@export var camera_bounds: Node2D
 @export var right_barrier_percent: float = 0.3
 @export var left_barrier_percent: float = 0.1
+
+var min_camera_x: float
+var max_camera_x: float
+
+func _ready() -> void:
+	var shape := camera_bounds.get_node("CollisionShape2d").shape as RectangleShape2D
+	var rect_size := shape.size
+	var rect_center := camera_bounds.global_position.x
+	var rect_width := shape.size.x
+	
+	# Half the width of the rectangle goes to the left, and other half goes to the right
+	min_camera_x = rect_center - rect_width / 2
+	max_camera_x = rect_center + rect_width / 2
 	
 func _process(delta: float) -> void:
 	if not player_node:
