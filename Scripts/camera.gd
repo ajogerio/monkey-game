cee732c65023dbe815f2b812861b77ec6bc57299
ajogerio <aaogerio@up.edu.ph>
2026-02-04 22:07:48 +0800

    feat: adjust max camera x

--- Scripts/camera.gd
+++ Scripts/camera.gd
@@ -17,7 +17,7 @@ func set_camera_bounds(bounds: Area2D) -> void:
 	
 	# Half the width of the rectangle goes to the left, and other half goes to the right
 	min_camera_x = rect_center - rect_width / 2
-	max_camera_x = rect_center + rect_width / 2
+	max_camera_x = (rect_center + rect_width / 2) - get_viewport_rect().size.x
 	print(rect_center)
 	print(rect_width)
 	print(min_camera_x)
@@ -32,7 +32,9 @@ func _process(delta: float) -> void:
 	var left_barrier_of_deadzone = global_position.x + (viewport_width * left_barrier_percent)
 	
 	# make the camera pan to the right whenever the player is past the right barrier of the deadzone
-	if player_node.global_position.x > right_barrier_of_deadzone || player_node.global_position.x < left_barrier_of_deadzone:
+	if player_node.global_position.x > right_barrier_of_deadzone and global_position.x < max_camera_x:
+		global_position.x += player_node.velocity.x * delta
+	elif player_node.global_position.x < left_barrier_of_deadzone and global_position.x > min_camera_x:
 		global_position.x += player_node.velocity.x * delta
 	
 	# make sure the camera stays within bounds
