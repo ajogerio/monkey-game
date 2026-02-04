6f88001becbc23be1deff830a7d6ccb39f917856
ajogerio <aaogerio@up.edu.ph>
2026-02-04 19:15:08 +0800

    feat: freeze player when talking to snorlax

--- Scripts/snorlax.gd
+++ Scripts/snorlax.gd
@@ -5,6 +5,7 @@ extends Area2D
 var is_player_in_talk_range := false
 var is_talking := false
 var checkpoint_chain_started := false
+var player_node
 
 func _process(delta: float) -> void:
 	if is_player_in_talk_range and !is_talking and Input.is_action_just_pressed("interact"):
@@ -21,6 +22,10 @@ func _on_body_exited(body: Node2D) -> void:
 		floating_e_key.visible = false
 
 func _start_dialogue():
+	var player = get_tree().get_first_node_in_group("player")
+	if player:
+		player_node = player
+		player_node.controls_enabled = false # unfreeze player
 	floating_e_key.visible = false
 	is_talking = true
 	DialogueManagerAutoload.dialogue_box.show_dialogue("res://Resources/tutorial_snorlax_meetup.json")
@@ -32,6 +37,7 @@ func _on_dialogue_box_dialogue_finished() -> void:
 	if not checkpoint_chain_started:
 		SignalBusAutoload.start_checkpoint_chain.emit("Checkpoint1")
 		checkpoint_chain_started = true
+	player_node.controls_enabled = true # unfreeze the player
 
 func _ready():
 	DialogueManagerAutoload.dialogue_finished.connect(_on_dialogue_box_dialogue_finished)
