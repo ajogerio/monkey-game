cfb97cff1fb41e8b8d8bb883f09c73e542ff932c
ajogerio <aaogerio@up.edu.ph>
2026-02-09 22:07:45 +0800

    feat: replace with signal

--- Scripts/exit_area.gd
+++ Scripts/exit_area.gd
@@ -1,11 +1,9 @@
 extends Area2D
+
 const JUNGLE_LEVEL: PackedScene = preload("res://Scenes/Levels/jungle_level_scene.tscn")
+signal load_next_level(next_level : PackedScene)
 
 func _on_body_entered(body: Node2D) -> void:
 	if body.is_in_group("player"):
-		change_level()
-		
-func change_level():
-	var level_manager = get_tree().get_nodes_in_group("level_manager")
-	if level_manager.size() > 0:
-		level_manager[0].load_level(JUNGLE_LEVEL);
+		load_next_level.emit(JUNGLE_LEVEL)
+	
