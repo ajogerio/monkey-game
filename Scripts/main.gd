9b93ac62fbc8d0630ed592933b425d8d2db031b4
ajogerio <aaogerio@up.edu.ph>
2026-02-09 22:11:36 +0800

    feat: add and connect signal listener in Main

--- Scripts/main.gd
+++ Scripts/main.gd
@@ -17,6 +17,10 @@ func load_level(level_scene: PackedScene):
 	var level_instance: Node = level_scene.instantiate()
 	level_container.add_child(level_instance)
 	
+	# listen for the next level signal
+	if level_instance.has_signal("load_next_level"):
+		level_instance.load_next_level.connect(_on_load_next_level)
+		
 	# assign camera bounds to camera
 	if level_instance.has_node("Camera Bounds"):
 		var camera_bounds_node = level_instance.get_node("Camera Bounds") as Area2D
@@ -27,3 +31,7 @@ func load_level(level_scene: PackedScene):
 		var spawn := level_instance.get_node("Player Spawn") as Marker2D
 		if spawn:
 			player.global_position = spawn.global_position
+
+func _on_load_next_level(next_level : PackedScene):
+	$Camera2D.reset_camera()
+	load_level(next_level)
