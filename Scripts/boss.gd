6e34773f7f3d37bc36c3e7109dc877eac44ed00b
ajogerio <aaogerio@up.edu.ph>
2026-02-19 22:24:14 +0800

    feat: make bullet disappear on boss hit

--- Scripts/boss.gd
+++ Scripts/boss.gd
@@ -4,6 +4,9 @@ signal boss_hit
 
 func _on_hitbox_area_entered(area: Area2D) -> void:
 	if area.is_in_group("cookie_bullet"):
-		# send signal to the level that the boss took damage
+		# make bullet disappear
+		area.queue_free()
+		
+		# send signal to the level that the boss was hit
 		boss_hit.emit()
 		
