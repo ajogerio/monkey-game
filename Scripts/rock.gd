4503a8351b82082000445f8d55e03063f84c821c
ajogerio <aaogerio@up.edu.ph>
2026-02-20 21:38:37 +0800

    feat: remove rock on player collision

--- Scripts/rock.gd
+++ Scripts/rock.gd
@@ -4,7 +4,16 @@ extends CharacterBody2D
 
 func _physics_process(delta: float) -> void:
 	velocity.x = -speed # negative means move left
-	move_and_slide()
+	
+	# remove rock if it hits player
+	var collision = move_and_collide(velocity * delta)
+	
+	if collision:
+		var collider = collision.get_collider()
+		
+		if collider.is_in_group("player"):
+			queue_free()
+	
 
 func _on_visible_on_screen_notifier_2d_screen_exited() -> void:
-	queue_free() # remove rock when it exits
+	queue_free() # remove rock when it exits screen
