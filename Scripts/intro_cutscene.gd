42376ea66b9bd932bbeaa8f29e70f68a983ae1af
ajogerio <aaogerio@up.edu.ph>
2026-03-06 21:37:22 +0800

    feat: add arguing intro dialogue

--- Scripts/intro_cutscene.gd
+++ Scripts/intro_cutscene.gd
@@ -1,12 +1,19 @@
 extends Node2D
 
+@export var intro_dialogue_filepath: String
+
 func play_intro(player):
 	# disable player controls
 	player.controls_enabled = false
 	
 	# set a timer for 3 seconds to soak up the scene
-	# play fighting arguing dialogue
+	await get_tree().create_timer(3.0).timeout
+	
+	# play arguing dialogue
+	DialogueManagerAutoload.dialogue_box.show_dialogue(intro_dialogue_filepath)
+	
 	# monkeys enter the scene
+	
 	# monkeys grab aj
 	# monkey dialogue plays
 	# monkeys run away with aj to the right of the screen
