2801a6474bafa62b020b6210519381077490bc14
ajogerio <aaogerio@up.edu.ph>
2026-03-10 19:54:19 +0800

    feat: edit theme

--- Scripts/scroll_container.gd
+++ Scripts/scroll_container.gd
@@ -1,11 +1,6 @@
 extends ScrollContainer
 
 
-# Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+	var vscroll = self.get_v_scroll_bar()
+	vscroll.visible = false
